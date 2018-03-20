7563607fb1eb79e930198ef7f8b9acd9612f30f3
syennamani <syennamani@tracfone.com>
2018-03-20 10:34:55 -0400

    Fixed sub service

--- app/src/main/java/com/trac/preload/accountservices/SubManager/SubscriptionService.java
+++ app/src/main/java/com/trac/preload/accountservices/SubManager/SubscriptionService.java
@@ -2,6 +2,7 @@ package com.trac.preload.accountservices.SubManager;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.JobIntentService;
 import android.telephony.SubscriptionInfo;
@@ -11,26 +12,27 @@ import android.widget.Toast;
 
 import java.util.List;
 
+
 /**
  * Created by syennamani on 3/19/2018.
  */
 
 public class SubscriptionService extends JobIntentService {
     private static final String TAG = "SubscriptionService";
-    static SubscriptionManager mSubscriptionManager = null;
+    SubscriptionManager mSubscriptionManager = null;
     static final int JOB_ID = 100126;
-    private static List<SubscriptionInfo> mSubInfoList = null;
+    private static Context mContext;
+    private List<SubscriptionInfo> mSubInfoList = null;
+    private static Handler mHandler;
 
     public static void enqueueWork(Context context, Intent work) {
         enqueueWork(context, SubscriptionService.class, JOB_ID, work);
         Log.d(TAG, "enqueueWork: ");
-        if(mSubscriptionManager == null) {
-            mSubscriptionManager = SubscriptionManager.from(context);
-            mSubscriptionManager.addOnSubscriptionsChangedListener(mOnSubscriptionsChangeListener);
-        }
+        mContext = context;
+        mHandler = new Handler();
     }
 
-    private static final SubscriptionManager.OnSubscriptionsChangedListener mOnSubscriptionsChangeListener
+    private final SubscriptionManager.OnSubscriptionsChangedListener mOnSubscriptionsChangeListener
             = new SubscriptionManager.OnSubscriptionsChangedListener() {
         @Override
         public void onSubscriptionsChanged() {
@@ -42,16 +44,27 @@ public class SubscriptionService extends JobIntentService {
 
     @Override
     public void onDestroy() {
+        Log.d(TAG, "Service destroyed");
         Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show();
-        mSubscriptionManager.removeOnSubscriptionsChangedListener(mOnSubscriptionsChangeListener);
     }
 
     @Override
     protected void onHandleWork(@NonNull Intent intent) {
+        if(mSubscriptionManager == null) {
+            mSubscriptionManager = SubscriptionManager.from(mContext);
+        }
         printSubscriptionDetails();
+        mSubscriptionManager.addOnSubscriptionsChangedListener(mOnSubscriptionsChangeListener);
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.v(TAG, "Serviec is running");
+            }
+        }, 2000);
+
     }
 
-    private static void printSubscriptionDetails(){
+    private void printSubscriptionDetails(){
         mSubInfoList = mSubscriptionManager.getActiveSubscriptionInfoList();
         for(SubscriptionInfo info: mSubInfoList){
             Log.v(TAG, ""+info.getSubscriptionId()+ "\n"+ info.getNumber()+"\n"+info.getDisplayName()+"\n"+info.getCarrierName());
